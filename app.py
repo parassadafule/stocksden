424d9d2681c015141871df7dd1f319bc0c911c91
Paras Sadafule <142837204+parassadafule@users.noreply.github.com>
2024-03-17 11:26:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ import numpy as np
 
 app = Flask(__name__, static_url_path='/static')
 
-# Load the pre-trained model
-with open('insurance_cost_model.pkl', 'rb') as model_file:
-    insurance_price_model = pickle.load(model_file)
-
-with open('insurance_plan_model.pkl', 'rb') as model_file:
-    insurance_plan_model = pickle.load(model_file)
 
 
 @app.route('/')
@@ -70,57 +64,6 @@ def insurance_price():
 def insurance_plan():
     return render_template('insurance_plan_form.html')
 
-
-
-@app.route('/insurance_plan', methods=['POST'])
-def insurance_plan_predict():
-    # Extract input parameters from the form
-    age = int(request.form.get('age'))
-    gender = 1 if request.form.get('gender') == 'female' else 0
-    chest_pain = int(request.form.get('chest_pain'))
-    blood_pressure = int(request.form.get('blood_pressure'))
-    cholesterol = int(request.form.get('cholesterol'))
-    fasting_sugar = int(request.form.get('fasting_sugar'))
-    rest_ecg = int(request.form.get('rest_ecg'))
-    max_heart_rate = int(request.form.get('max_heart_rate'))
-    exercise_angina = 1 if request.form.get('exercise_angina') == 'true' else 0
-    st_depression = float(request.form.get('st_depression'))
-    slope_peak_st = int(request.form.get('slope_peak_st'))
-    num_vessels = int(request.form.get('num_vessels'))
-    thal = int(request.form.get('thal'))
-    # smoker = 1 if request.form.get('smoker') == 'true' else 0
-
-    # Convert input values to a NumPy array
-    input_data = np.array([[age, gender, chest_pain, blood_pressure, cholesterol, fasting_sugar, rest_ecg,max_heart_rate, exercise_angina, st_depression, slope_peak_st, num_vessels, thal]])
-
-    # Make a prediction using the trained model
-    prediction =insurance_plan_model.predict(input_data)
-
-    # Display the prediction
-    return render_template('insurance_plan_result.html', prediction=prediction[0])
-
-
-@app.route('/insurance_cost', methods=['POST'])
-def insurance_price_predict():
-    # Retrieve form data
-    age = int(request.form.get('age'))
-    sex = request.form.get('sex') == 'true'
-    bmi = float(request.form.get('bmi'))
-    children = int(request.form.get('children'))
-    smoker = request.form.get('smoker') == 'true'
-    region = int(request.form.get('region'))
-
-    # Create a feature vector
-    features = np.array([[age, sex, bmi, children, smoker, region]])
-    # Make a prediction using the pre-trained model
-    prediction = insurance_price_model.predict(features)
-    # For now, let's just print the prediction
-    print('Prediction:', prediction)
-    # You can return the prediction as JSON or any other format
-    return render_template('insurance_price_result.html', prediction=prediction[0])
-
-
-   
 
 if __name__ == '__main__':
     app.run(debug=True)
