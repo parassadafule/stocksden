83d71b53a9ee728c4389ea8cd3c3b03cd873a751
Paras Sadafule <142837204+parassadafule@users.noreply.github.com>
2024-03-17 10:46:45 +0530

    Delete sources directory

--- sources/resources/javascript/scratch_page.js
+++
@@ -1,8 +0,0 @@
-var num = Math.floor(Math.random() * 4) + 1;
-
-$("#card").wScratchPad({
-  size: 150, // The size of the brush/scratch.
-  bg: `Images/bg.jpg`, // Background (image path or hex color).
-  fg: `Images/back.jpg`, // Foreground (image path or hex color).
-  cursor: "pointer", // Set cursor.
-});
